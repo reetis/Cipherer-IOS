0bf9ab44e0c33c71f476f8a93afc6ccddc59f15d
Rytis Stankus <ryst0973@obuolys03.mif>
2014-12-02 12:33:53 +0200

    Initial Commit

---
+++ Cipherer-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cipherer-IOS
+//
+//  Created by Rytis Stankus on 12/2/14.
+//  Copyright (c) 2014 Rytis Stankus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
